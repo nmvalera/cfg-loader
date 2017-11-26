a0a16d53c4a06b5f7773b274f602456d646d4663
Nicolas Maurice <ricemo1989@msn.com>
2017-11-26 22:03:57 +0100

    add setup files

---
+++ setup.py
@@ -0,0 +1,50 @@
+"""
+    Boilerplate-Python
+    ~~~~~~~~~~~~~~~~~~
+
+    Boilerplate-Python is an empty Python project.
+
+    It is meant to be forked when starting a new Python project to inherit multiple DevOps functions.
+"""
+
+import os
+
+from setuptools import setup
+
+
+def read(file_name):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+    except FileNotFoundError:
+        return ''
+
+
+setup(
+    name='Boilerplate-Python',
+    version='0.0.0',
+    license=read('LICENSE.rst'),
+    url='https://gitlab.com/nicolas.maurice.valera/boilerplate-python',
+    author='Nicolas Maurice',
+    author_email='nicolas.maurice.valera@gmail.com',
+    description='Boilerplate-Python is an empty Python project',
+    packages=['boilerplate_python'],
+    install_requires=[
+    ],
+    extras_require={
+        'dev': [
+            'autoflake',
+            'autopep8',
+            'flake8',
+        ],
+    },
+    zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests'
+)
