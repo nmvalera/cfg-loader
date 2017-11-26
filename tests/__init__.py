756c441e31b27e0d30b05c7c7f7ad61aaac682b2
Nicolas Maurice <ricemo1989@msn.com>
2017-11-26 22:00:33 +0100

    add test volume

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+    tests
+    ~~~~~
+
+    Test suite for Boilerplate-Python project
+
+    :copyright: (c) 2017 by Nicolas Maurice.
+    :license: BSD, see :ref:`license` for more details.
+"""
