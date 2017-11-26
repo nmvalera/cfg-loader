42c18953d4a41947c1ca019a3643dd5c1e5a82da
Nicolas Maurice <ricemo1989@msn.com>
2017-11-26 22:00:21 +0100

    add main module

---
+++ boilerplate_python/__init__.py
@@ -0,0 +1,11 @@
+"""
+    boilerplate
+    ~~~~~~~~~~~
+
+    An empty python project
+
+    :copyright: (c) 2017 by Nicolas Maurice.
+    :license: BSD, see :ref:`license` for more details.
+"""
+
+__version__ = '0.0.0'
