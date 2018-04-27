bc1c16ece43b282138c3d45f3616d514d88b5c17
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-04-27 10:12:21 +0200

    feat(fields): implement custom marshmallow field validating against a path

---
+++ config_loader/fields.py
@@ -0,0 +1,54 @@
+"""
+    config_loader.fields
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Implement marshmallow fields to validate against specific input data
+
+    :copyright: Copyright 2017 by ConsenSys France.
+    :license: BSD, see :ref:`license` for more details.
+"""
+
+import os
+
+from marshmallow import validate, fields
+
+
+class PathValidator(validate.Validator):
+    """Validate a path.
+
+    :param error: Error message to raise in case of a validation error. Can be
+        interpolated with `{input}`.
+    :type error: str
+    """
+
+    default_message = 'Path "{input}" does not exist'
+
+    def __init__(self, error=None):
+        self.error = error or self.default_message
+
+    def _format_error(self, value):
+        return self.error.format(input=value)
+
+    def __call__(self, value):
+        message = self._format_error(value)
+
+        if not os.path.exists(value):
+            raise validate.ValidationError(message)
+
+        return value
+
+
+class Path(fields.String):
+    """A validated path field. Validation occurs during both serialization and
+    deserialization.
+
+    :param args: The same positional arguments that :class:`~marshmallow.fields.String` receives.
+    :param kwargs: The same keyword arguments that :class:`~marshmallow.fields.String` receives.
+    """
+
+    default_error_messages = {'invalid_path': 'Path "{input}" does not exist'}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Insert validation into self.validators so that multiple errors can be
+        self.validators.append(PathValidator(error=self.error_messages['invalid_path']))
