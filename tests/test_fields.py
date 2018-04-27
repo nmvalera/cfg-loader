51b4eec10690d9b6f4746db48d701de584ad9021
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-04-27 10:13:05 +0200

    test(fields): implement test for custom path field

---
+++ tests/test_fields.py
@@ -0,0 +1,32 @@
+"""
+    tests.test_utils
+    ~~~~~~~~~~~~~~~~
+
+    Test fields
+
+    :copyright: Copyright 2017 by ConsenSys France.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import pytest
+from config_loader.fields import PathValidator, Path
+from marshmallow import Schema, ValidationError
+
+
+def test_path_validator(config_path):
+    validator = PathValidator()
+    with pytest.raises(ValidationError):
+        validator('invalid')
+    assert validator(config_path) == config_path
+
+
+def test_path_field(config_path):
+    class SchemaTest(Schema):
+        path_field = Path()
+
+    data = {
+        'path_field': config_path,
+    }
+    assert SchemaTest().load(data) == {
+        'path_field': config_path,
+    }
