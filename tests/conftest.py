02945c93f84ae59634318773f3664e91678d0a4a
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-04-27 10:11:53 +0200

    tests(loader): implement test for loaders

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,8 +12,50 @@ import pytest
 
 TEST_DIR = os.path.dirname(__file__)
 CONFIG_DIR = os.path.join(TEST_DIR, 'config')
+BASE_CONFIG_PATH = os.path.join(CONFIG_DIR, 'config.yml')
 
 
-@pytest.fixture(scope='session')
-def config_path():
-    yield os.path.join(CONFIG_DIR, 'config.yml')
+def set_env_vars(env_vars_to_set):
+    """Set environment variable to a value and returns a function to reset environment variables to their old value
+
+    :param env_vars_to_set: List of tuples `(var, new_value)` of environment variables to set
+    :type env_vars_to_set: list
+
+    Example:
+    >>> import os
+    >>> os.environ['VARIABLE'] = 'value'
+    >>> reset_env_vars = set_env_vars([('VARIABLE', 'new_value')])
+    >>> os.environ['VARIABLE']
+    'new_value'
+    >>> reset_env_vars()
+    >>> os.environ['VARIABLE']
+    'value'
+    """
+
+    vars = [(var[0], var[1], os.environ.get(var[0])) for var in env_vars_to_set]
+
+    # Set environment variables
+    for var, new_value, old_value in vars:
+        if new_value is None and old_value is not None:
+            del os.environ[var]
+        elif new_value is not None:
+            os.environ[var] = new_value
+
+    def reset_env_vars():
+        set_env_vars([(var[0], var[2]) for var in vars])
+
+    return reset_env_vars
+
+
+@pytest.fixture
+def env_vars():
+    yield []
+
+
+@pytest.fixture
+def config_path(env_vars):
+    reset_env_vars = set_env_vars(env_vars)
+
+    yield BASE_CONFIG_PATH
+
+    reset_env_vars()
---
+++ tests/test_loader.py
@@ -0,0 +1,54 @@
+"""
+    tests.test_loader
+    ~~~~~~~~~~~~~~~~~
+
+    Test configuration loader
+
+    :copyright: Copyright 2017 by ConsenSys France.
+    :license: BSD, see :ref:`license` for more details.
+"""
+import pytest
+from config_loader.exceptions import ConfigFileMissingError, ConfigFileNotFoundError
+from config_loader.loader import BaseConfigLoader, YamlBaseConfigLoader
+from marshmallow import Schema, fields
+
+from .conftest import BASE_CONFIG_PATH
+
+
+class ConfigSchemaTest(Schema):
+    setting = fields.Str()
+
+
+def test_base_config_loader():
+    config_loader = BaseConfigLoader(ConfigSchemaTest())
+    assert config_loader.load({'setting': 'value'}) == {'setting': 'value'}
+
+
+def test_yaml_config_loader(config_path):
+    config_loader = YamlBaseConfigLoader(ConfigSchemaTest())
+
+    assert config_loader.load(config_path) == {'setting': 'setting_value'}
+
+
+@pytest.mark.parametrize('env_vars', [[('VARIABLE', BASE_CONFIG_PATH)]])
+def test_yaml_config_loader_env_var(config_path):
+    config_loader = YamlBaseConfigLoader(ConfigSchemaTest(), config_file_env_var='VARIABLE')
+
+    assert config_loader.load() == {'setting': 'setting_value'}
+
+
+@pytest.mark.parametrize('env_vars', [[('VARIABLE', None)]])
+def test_yaml_config_loader_no_path(config_path):
+    config_loader = YamlBaseConfigLoader(ConfigSchemaTest(), config_file_env_var='VARIABLE')
+
+    with pytest.raises(ConfigFileMissingError) as e:
+        config_loader.load()
+    assert 'VARIABLE' in str(e.value)
+
+
+def test_yaml_config_loader_invalid_path():
+    config_loader = YamlBaseConfigLoader(ConfigSchemaTest())
+
+    with pytest.raises(ConfigFileNotFoundError) as e:
+        config_loader.load('unknown/config/file')
+    assert 'unknown/config/file' in str(e.value)
