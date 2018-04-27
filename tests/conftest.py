32d266efeca3fe230562979e378ede46831c266a
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-04-27 09:17:42 +0200

    tests(conftest): implement basic fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,15 @@
     :copyright: Copyright 2017 by ConsenSys France.
     :license: BSD, see LICENSE for more details.
 """
+
+import os
+
+import pytest
+
+TEST_DIR = os.path.dirname(__file__)
+CONFIG_DIR = os.path.join(TEST_DIR, 'config')
+
+
+@pytest.fixture(scope='session')
+def config_path():
+    yield os.path.join(CONFIG_DIR, 'config.yml')
