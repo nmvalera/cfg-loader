17f975518055f7b92257f4c9633a6b5cad9a9e4c
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-04-27 15:09:49 +0200

    tests(loader): update tests for new ConfigLoader init API

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -10,36 +10,37 @@
 import pytest
 from config_loader.exceptions import ConfigFileMissingError, ConfigFileNotFoundError
 from config_loader.loader import BaseConfigLoader, YamlBaseConfigLoader
-from marshmallow import Schema, fields
+from config_loader.schema import ConfigSchema
+from marshmallow import fields
 
 from .conftest import BASE_CONFIG_PATH
 
 
-class ConfigSchemaTest(Schema):
+class ConfigSchemaTest(ConfigSchema):
     setting = fields.Str()
 
 
 def test_base_config_loader():
-    config_loader = BaseConfigLoader(ConfigSchemaTest())
+    config_loader = BaseConfigLoader(ConfigSchemaTest)
     assert config_loader.load({'setting': 'value'}) == {'setting': 'value'}
 
 
 def test_yaml_config_loader(config_path):
-    config_loader = YamlBaseConfigLoader(ConfigSchemaTest())
+    config_loader = YamlBaseConfigLoader(ConfigSchemaTest)
 
     assert config_loader.load(config_path) == {'setting': 'setting_value'}
 
 
 @pytest.mark.parametrize('env_vars', [[('VARIABLE', BASE_CONFIG_PATH)]])
-def test_yaml_config_loader_env_var(config_path):
-    config_loader = YamlBaseConfigLoader(ConfigSchemaTest(), config_file_env_var='VARIABLE')
+def test_yaml_config_loader_from_env_var(config_path):
+    config_loader = YamlBaseConfigLoader(ConfigSchemaTest, config_file_env_var='VARIABLE')
 
     assert config_loader.load() == {'setting': 'setting_value'}
 
 
 @pytest.mark.parametrize('env_vars', [[('VARIABLE', None)]])
 def test_yaml_config_loader_no_path(config_path):
-    config_loader = YamlBaseConfigLoader(ConfigSchemaTest(), config_file_env_var='VARIABLE')
+    config_loader = YamlBaseConfigLoader(ConfigSchemaTest, config_file_env_var='VARIABLE')
 
     with pytest.raises(ConfigFileMissingError) as e:
         config_loader.load()
@@ -47,7 +48,7 @@ def test_yaml_config_loader_no_path(config_path):
 
 
 def test_yaml_config_loader_invalid_path():
-    config_loader = YamlBaseConfigLoader(ConfigSchemaTest())
+    config_loader = YamlBaseConfigLoader(ConfigSchemaTest)
 
     with pytest.raises(ConfigFileNotFoundError) as e:
         config_loader.load('unknown/config/file')
