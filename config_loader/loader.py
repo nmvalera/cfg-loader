9e64cac5a978718c6f8ff25d9a32e795ba06630b
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-04-27 09:18:19 +0200

    feat(loader): implement configuration loader

---
+++ config_loader/loader.py
@@ -0,0 +1,88 @@
+"""
+    config_loader.loader
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Implement the loading class
+
+    :copyright: Copyright 2017 by ConsenSys France.
+    :license: BSD, see :ref:`license` for more details.
+"""
+
+import os
+
+from .exceptions import ConfigFileMissingError, ConfigFileNotFoundError
+from .utils import parse_yaml_file
+
+# Default environment variable containing the path of a .yaml configuration file
+DEFAULT_CONFIG_FILE_ENV_VAR = 'CONFIG_FILE'
+
+
+class BaseConfigLoader:
+    """Base config loader using a marshmallow schema to validate and process input data
+
+    :param schema: Marshmallow schema used to deserialize configuration input data
+    """
+
+    def __init__(self, schema):
+        self.schema = schema
+
+    def load(self, data):
+        """Load configuration from an object
+
+        :param input: Data to load configuration from (must be deserializable by schema)
+        :type input: object
+        """
+
+        return self.schema.load(data)
+
+
+class YamlBaseConfigLoader(BaseConfigLoader):
+    """Config loader that reads config from .yaml file
+
+    :param config_file_env_var: Environment variable to read config file path from if not provided when loading
+    :type config_file_env_var: str
+    """
+
+    def __init__(self, *args, config_file_env_var=DEFAULT_CONFIG_FILE_ENV_VAR, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.config_file_env_var = config_file_env_var
+
+    def check_file(self, config_file):
+        """Check file validity
+
+        :param config_file: Path to the .yaml configuration file
+        :type config_file: str
+        """
+
+        if config_file is None:
+            raise ConfigFileMissingError("""
+                    No configuration file specified.
+                    Please provide a configuration file path or 
+                    set the environ variable '{var}'
+                """.format(var=self.config_file_env_var))
+
+        if not os.path.isfile(config_file):
+            raise ConfigFileNotFoundError("""
+                    No such file '{path}'
+                """.format(path=config_file))
+
+        return config_file
+
+    def load(self, config_file=None):
+        """Load configuration from .yaml file
+
+        :param config_file: Path to the .yaml configuration file
+        :type config_file: str
+        """
+
+        if config_file is None:
+            # If config file is not provided retrieves it from environment variable
+            config_file = os.environ.get(self.config_file_env_var)
+
+        # Check config_file is valid
+        self.check_file(config_file)
+
+        # Parse yaml file
+        input = parse_yaml_file(config_file)
+
+        return super().load(input)
